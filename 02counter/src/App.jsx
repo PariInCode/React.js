7e643dfc36edf4771ea0f57f2e12ed316e662378
PariInCode <parisaxena713@gmail.com>
2026-01-01 16:05:17 +0530

    .

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import{usestate}from'react'
+
+
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //change state in DOM
+
+ let[counter,setCounter] = useState(5)
+
+//counter is variable and setcounter is variable update function
+
+ // let  counter = 5 
+
+  const addvalue = ()=>{
+    
+    //counter=counter+1
+    setCounter(counter+1)
+  }
+
+  const removevalue = ()=>{
+    setCounter(counter-1)
+  }
+  return (
+    <>
+      <h1>just react</h1>
+      <h2>Counter value:counter</h2>
+
+      <button onClick={addvalue}>add value{counter}</button>
+      <br />
+      <button onClick={removevalue}>remove value{counter}</button>
+    </>
+  )
+}
+
+export default App
