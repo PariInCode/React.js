5b23d7df2d9ce6d90830b9a26a6bf2212d27a947
PariInCode <parisaxena713@gmail.com>
2026-01-13 17:54:29 +0530

    .

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
+import { ThemeProvider } from './Context/theme';
+import ThemeBtn from './Components/ThemeBtn';
+import Card from './Components/Card';
+
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const darkTheme = () => { setThemeMode("dark") };
+  const lightTheme = () => { setThemeMode("light") };
+
+
+  //Actual themechnge 
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('Light', 'Dark');
+    document.querySelector('html').classList.add(themeMode);
+  })
+  return (
+    <>
+      <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
